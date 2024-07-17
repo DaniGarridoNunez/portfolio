5494be3147693f670dd0c5ac0a32a671a07e2323
Dani Garrido <83754027+DaniGarridoNunez@users.noreply.github.com>
2024-07-17 10:16:41 +0200

    Temporary image fix and fixed overflox hidden

--- app/(routes)/cv/page.tsx
+++ app/(routes)/cv/page.tsx
@@ -7,7 +7,7 @@ const ServicesPage = () => {
         <>
 
             <TransitionPage />
-            <CircleImage />
+            {/* <CircleImage /> */}
             <div className="flex flex-col items-center justify-center h-screen max-w-5xl mx-auto">
                 <div className="max-w-[450px]">
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">My <span className="font-bold text-secondary"> CV</span></h1>
